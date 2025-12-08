5ce8c4f24d0696e8b12580a7010bc71c9fa55335
Ori Bermudez <ori.bermudez@outlook.com>
2025-12-08 16:14:54 -0700

    Fixed sidebar

--- src/components/Layouts/sidebar/index.tsx
+++ src/components/Layouts/sidebar/index.tsx
@@ -29,14 +29,7 @@ export function Sidebar() {
     NAV_DATA.some((section) => {
       return section.items.some((item) => {
         return item.items.some((subItem) => {
-          if (subItem.url === pathname) {
-            if (!expandedItems.includes(item.title)) {
-              toggleExpanded(item.title);
-            }
-
-            // Break the loop
-            return true;
-          }
+          
         });
       });
     });
